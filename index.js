522391977382117dbf3e9d4c704d1b873d6e3afc
unknown <perso-29@protonmail.com>
2020-10-14 23:03:08 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/* Valores
+
+enteros o inters: todos los operadores logicos numeros
+123.... 12.. 14
+
+decimales o float: los que terminan con punto son decimales.
+12.14
+o 
+11.43
+
+cadena o string: cadenas de texto dentro de los parentesis
+"A"
+'a'
+
+Boolean: Solamente es verdadero o false no almacenan dentro de los parentesis
+true 
+false */
+
+/* Operadores aritmeticos */
+
+/* siempre toma como prioridad la multiplicacion y la division antes que la suma y la resta etc.antes 
+ para sacar un residuo de un numero no divisible se saca atra vez  del porcentaje %
+ console.log(37%5);
+37
+35
+ 2
+*/
+
+console.log(- (10-2));
